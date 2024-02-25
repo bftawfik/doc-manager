b42a5131d070ea136b7557bdfc481f4db022a60e
sara eid <sara.eid.77777@gmail.com>
2024-02-25 10:59:42 +0200

    #23 add generic dropdowns

--- src/app/_components/dropDownList/DropDownList.tsx
+++ src/app/_components/dropDownList/DropDownList.tsx
@@ -14,23 +14,34 @@ interface DropDownListProps {
   label?: string | React.ReactNode;
   icon?: React.ReactNode;
   list: Item[];
+  labelClassName?: string;
+  contentClassName?: string;
 }
-const DropDownList = ({ list, icon, label }: DropDownListProps) => {
+const DropDownList = ({
+  list,
+  icon,
+  label,
+  labelClassName = "flex items-center gap-1",
+  contentClassName = "min-w-fit",
+}: DropDownListProps) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger
         asChild
         className="cursor-pointer focus:outline-none "
       >
-        <button className="flex items-center gap-1">
+        <button className={labelClassName}>
           {label && <span className="text-sm text-black/65">{label}</span>}
           {icon && icon}
         </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="min-w-fit">
+      <DropdownMenuContent align="end" className={contentClassName}>
         {list.map(({ id, icon, handler, title }) => (
           <DropdownMenuItem key={id}>
-            <button className="flex items-center gap-1" onClick={handler}>
+            <button
+              className="flex w-full items-center gap-1"
+              onClick={handler}
+            >
               {icon && icon}
               <span className="text-sm capitalize">{title}</span>
             </button>
